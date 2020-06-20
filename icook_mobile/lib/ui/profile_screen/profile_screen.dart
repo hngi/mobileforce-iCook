96165d0e089b87a9ca819148deea5a0b8b5692b8
gbolahan146 <gbolahanoduyemi1@gmail.com>
2020-06-20 18:44:48 +0100

    #feat: updated profile screen

--- icook_mobile/lib/ui/profile_screen/profile_screen.dart
+++ icook_mobile/lib/ui/profile_screen/profile_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:icook_mobile/ui/profile_screen/constant.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 import '../ui_helper.dart';
@@ -14,19 +13,19 @@ class ProfileScreen extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         SizedBox(width: 1),
-        Icon(
-          LineAwesomeIcons.angle_left,
-          size: 31,
-        ),
-        SizedBox(width: Constants.kSpacingUnit.w * 2),
+        Image(image: new AssetImage('assets/images/Vector.png',), height: 35,),
+        
+        SizedBox(width: Constants.kSpacingUnit.w * 5),
         Text(
           'My profile',
-          style: GoogleFonts.workSans(
+          style: GoogleFonts.poppins(
               textStyle: TextStyle(color: Colors.black, fontSize: 24),
               fontWeight: FontWeight.w500),
         ),
-        SizedBox(width: Constants.kSpacingUnit.w * 4.2),
-        Icon(Icons.search, size: 30),
+        SizedBox(width: Constants.kSpacingUnit.w * 5),
+        
+        Image(image: new AssetImage('assets/images/search.png',), height: 35,),
+
         SizedBox(width: 0.1),
       ],
     );
@@ -34,24 +33,31 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          SizedBox(height: kSpacingUnit.w * 5),
+          SizedBox(height: Constants.kSpacingUnit.w * 6),
           header,
           Expanded(
-            child: ListView(
+           child: ListView(
               children: <Widget>[
                 ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: AssetImage('assets/images/avatar.png'),
-                  ),
+                  leading: Container(
+                      width: 60,
+                      height: 90,
+                      decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.cover,
+                              image:
+                                  new AssetImage('assets/images/avatar.png')))),
                   title: Text(
                     'Chef Emmanuel',
-                    style: GoogleFonts.workSans(
-                        textStyle: TextStyle(color: Colors.black, fontSize: 24),
-                        fontWeight: FontWeight.bold),
+                    style: GoogleFonts.poppins(
+                        textStyle:
+                            TextStyle(color: Color(0xFF222222), fontSize: 18),
+                        fontWeight: FontWeight.w600),
                   ),
                   subtitle: Text(
                     'Emmanuelchef@gmail.com',
-                    style: GoogleFonts.workSans(
+                    style: GoogleFonts.poppins(
                         textStyle:
                             TextStyle(color: Colors.blueGrey, fontSize: 14)),
                   ),
@@ -59,142 +65,119 @@ class ProfileScreen extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(1.0),
-                  ),
-                  child: ListTile(
+                
+               ListTile(
                     title: Text(
                       'Account Information',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle: TextStyle(
                               color: Colors.black,
-                              fontSize: 24,
+                              fontSize: 16,
                               fontWeight: FontWeight.w500)),
                     ),
                     subtitle: Text(
                       'Edit profile',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle:
-                              TextStyle(color: Colors.blueGrey, fontSize: 14)),
+                              TextStyle(color: Colors.blueGrey, fontSize: 12)),
                     ),
                     trailing: Icon(Icons.keyboard_arrow_right),
                     onTap: () {
                       print('Account Information');
                     },
                   ),
-                ),
                 SizedBox(
                   height: 5,
                 ),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(1.0),
-                  ),
-                  child: ListTile(
+                ListTile(
                     title: Text(
                       'My Dishes',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle: TextStyle(
                               color: Colors.black,
-                              fontSize: 24,
+                              fontSize: 16,
                               fontWeight: FontWeight.w500)),
                     ),
                     subtitle: Text(
                       '4 dishes posted',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle:
-                              TextStyle(color: Colors.blueGrey, fontSize: 14)),
+                              TextStyle(color: Colors.blueGrey, fontSize: 12)),
                     ),
                     trailing: Icon(Icons.keyboard_arrow_right),
                     onTap: () {
                       print('my dishes');
                     },
                   ),
-                ),
+
                 SizedBox(
                   height: 5,
                 ),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(1.0),
-                  ),
-                  child: ListTile(
+            ListTile(
                     title: Text(
                       'Notifications',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle: TextStyle(
                               color: Colors.black,
-                              fontSize: 24,
+                              fontSize: 16,
                               fontWeight: FontWeight.w500)),
                     ),
                     subtitle: Text(
                       '10 pending notifications',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle:
-                              TextStyle(color: Colors.blueGrey, fontSize: 14)),
+                              TextStyle(color: Colors.blueGrey, fontSize: 12)),
                     ),
                     trailing: Icon(Icons.keyboard_arrow_right),
                     onTap: () {
                       print('notification');
                     },
                   ),
-                ),
                 SizedBox(
                   height: 5,
                 ),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(1.0),
-                  ),
-                  child: ListTile(
+                ListTile(
                     title: Text(
                       'My Reviews',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle:
-                              TextStyle(color: Colors.black, fontSize: 24),
+                              TextStyle(color: Colors.black, fontSize: 16),
                           fontWeight: FontWeight.w500),
                     ),
                     subtitle: Text(
                       'Reviews for 4 recipe',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle:
-                              TextStyle(color: Colors.blueGrey, fontSize: 14)),
+                              TextStyle(color: Colors.blueGrey, fontSize: 12)),
                     ),
                     trailing: Icon(Icons.keyboard_arrow_right),
                     onTap: () {
                       print('reviews');
                     },
                   ),
-                ),
                 SizedBox(
                   height: 5,
                 ),
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(1.0),
-                  ),
-                  child: ListTile(
+                  ListTile(
                     title: Text(
                       'Settings',
-                      style: GoogleFonts.workSans(
+                      style: GoogleFonts.poppins(
                           textStyle: TextStyle(
                               color: Colors.black,
-                              fontSize: 24,
+                              fontSize: 16,
                               fontWeight: FontWeight.w500)),
                     ),
                     subtitle: Text(
-                      'Newsletter, linked accounts, Change Password',
-                      style: GoogleFonts.workSans(
+                      'Newsletter, Dark mode, Change Password',
+                      style: GoogleFonts.poppins(
                           textStyle:
-                              TextStyle(color: Colors.blueGrey, fontSize: 14)),
+                              TextStyle(color: Colors.blueGrey, fontSize: 12)),
                     ),
                     trailing: Icon(Icons.keyboard_arrow_right),
                     onTap: () {
                       print('reviews');
                     },
                   ),
-                ),
                 SizedBox(
                   height: 10,
                 ),
