95ebff1e3283d12b99fccab441c8bf2744cf5519
lazicah <lazicah@gmail.com>
2020-06-21 13:42:48 +0100

    update

--- icook_mobile/lib/ui/splash_screen/splash_screen.dart
+++ icook_mobile/lib/ui/splash_screen/splash_screen.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:icook_mobile/ui/splash_screen/splash_model.dart';
 import 'package:icook_mobile/ui/widgets/k_button.dart';
+import 'package:stacked/stacked.dart';
 
 import '../ui_helper.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(
-              image: AssetImage('assets/images/icook_logo.png'),
-            ),
-            SizedBox(height: 100),
-            KButton(
-                title: 'Get Started',
-                buttonColor: Constants.kbuttonColor1,
-                textColor: Colors.white),
-          ],
+    return ViewModelBuilder<SplashModel>.nonReactive(
+      viewModelBuilder: () => SplashModel(),
+      onModelReady: (model) => model.init(),
+      builder: (context, model, child) => Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage('assets/images/icook_logo.png'),
+              ),
+              SizedBox(height: 100),
+              KButton(
+                  onPressed: () => model.getStarted(),
+                  title: 'Get Started',
+                  buttonColor: Constants.kbuttonColor1,
+                  textColor: Colors.white),
+            ],
+          ),
         ),
       ),
     );
