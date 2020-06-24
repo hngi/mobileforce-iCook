3f0953723ca41649cd382465ba96e578801ee282
Diczen <62258868+Diczen@users.noreply.github.com>
2020-06-24 20:59:05 +0100

    Commits for Login Screen

---
+++ icook_mobile/lib/ui/ui_helper/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+final kHintTextStyle = TextStyle(
+  color: Colors.grey ,
+  fontFamily: 'Poppins',
+
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'Poppins',
+
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Colors.black12,
+  borderRadius: BorderRadius.circular(10.0),
+  );
