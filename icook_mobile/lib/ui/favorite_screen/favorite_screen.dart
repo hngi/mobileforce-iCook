2435d1fa26bcd87b1c8755b4be52b7eb373cbd1b
presheda <preciousedafe15@gmail.com>
2020-07-01 23:18:04 +0100

    Created Favorite Model

--- icook_mobile/lib/ui/favorite_screen/favorite_screen.dart
+++ icook_mobile/lib/ui/favorite_screen/favorite_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:icook_mobile/models/response/Dish/getmydishes.dart';
+import 'package:icook_mobile/ui/favorite_screen/favorite_model.dart';
 
 import 'package:icook_mobile/ui/shared/recipe_item.dart';
+import 'package:icook_mobile/ui/shared/state_responsive.dart';
+import 'package:stacked/stacked.dart';
 
 class FavoriteScreen extends StatefulWidget {
   @override
@@ -12,39 +15,37 @@ class FavoriteScreen extends StatefulWidget {
 class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Favorite"),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 20,
-              right: 20,
-            ),
-            child: InkWell(
-              onTap: () {},
-              child: Image(
-                image: AssetImage("assets/images/search_fav.png"),
-                width: 24,
-                height: 24,
+    return ViewModelBuilder<FavoriteScreenModel>.reactive(
+      viewModelBuilder: () => FavoriteScreenModel(),
+      onModelReady: (model) => model.init(),
+      builder: (context, model, child) => Scaffold(
+          appBar: AppBar(
+            title: Text("Favorite"),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  child: Image(
+                    image: AssetImage("assets/images/search_fav.png"),
+                    width: 24,
+                    height: 24,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          body: StateResponsive(
+            dataFetchedWidget: ListView.builder(
+              itemCount: model.dishList.length,
+              itemBuilder: (context, index) => RecipeItem(
+                dish: model.dishList[index]
               ),
             ),
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: 6,
-        itemBuilder: (context, index) => RecipeItem(
-          dish: Dishe(
-              name: 'Garri',
-              likesCount: 76,
-              isLiked: false,
-              recipe: ['cook and boil', 'bless the meal'],
-              dishImages: ['assets/images/amala.jpeg'],
-              ingredients: ['1 spoon of garri', '2 basins of sugar'],
-              healthBenefits: ['Good for Athrirtis']),
-        ),
-      ),
+          )),
     );
   }
 }
